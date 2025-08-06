8e01366599ba5ca4f5d08e2ed9c00560b9383fb9
Vachek-hooves <vachek0602@gmail.com>
2025-08-06 18:08:57 +0300

    on test

---
+++ config/credentials.js
@@ -0,0 +1,28 @@
+// appFlyer initSdk options
+
+export const OPTIONS = {
+  devKey: 'xR8fRQMXpbrbce5hB9a5iY',
+  // appId: '',
+  onInstallConversionDataListener: true,
+  onDeepLinkListener: true,
+  timeToWaitForATTUserAuthorization: 10,
+  manualStart: true,
+};
+
+// secret FB key - b78fa087d67424a4f80de4a1ac6737dd
+
+// FB app cred
+export const FB_APP_ID = '4014887552157658';
+export const FB_CLIENT_TOKEN = 'e8e42c64d3c45ae724c901d4d55ecdcc';
+
+// Compaign
+// https://ember-ash-valley.space/Jne1vy3r
+export const INITIAL_URL = `https://ember-ash-valley.space/`;
+export const URL_IDENTIFAIRE = `Jne1vy3r`;
+
+export const ONE_SIGNAL_ID = '8de2b438-3c4e-4913-bc63-b7f161ab6d62';
+
+export const USER_AGENT =
+  '"Mozilla/5.0 (Linux; Android 13; Mobile) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36"';
+
+// s2s token - 2cc93fb3-9c1b-438a-8d36-1d20f10e77b8
